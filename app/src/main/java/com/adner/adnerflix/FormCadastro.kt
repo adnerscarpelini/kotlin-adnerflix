2e42f543a7b1b620019a78375c958a52106de9da
Adner Scarpelini <adner.scarpelini@gmail.com>
2021-07-08 08:35:32 -0300

    Tela de Login

---
+++ app/src/main/java/com/adner/adnerflix/FormCadastro.kt
@@ -0,0 +1,11 @@
+package com.adner.adnerflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+    }
+}
--- app/src/main/java/com/adner/adnerflix/FormLogin.kt
+++ app/src/main/java/com/adner/adnerflix/FormLogin.kt
@@ -1,11 +1,29 @@
 package com.adner.adnerflix
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.adner.adnerflix.databinding.ActivityFormLoginBinding
 
 class FormLogin : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_login)
+
+        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Remover a barra de status
+        supportActionBar!!.hide()
+
+
+        binding.txtTelaCadastro.setOnClickListener{
+            val intent = Intent(this,FormCadastro::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

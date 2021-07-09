4232043bcedb897fb9a3d5fee72d77b921382cf3
Adner Scarpelini <adner.scarpelini@gmail.com>
2021-07-09 15:09:13 -0300

    Tela de Login

--- app/src/main/java/com/adner/adnerflix/FormCadastro.kt
+++ app/src/main/java/com/adner/adnerflix/FormCadastro.kt
@@ -2,10 +2,24 @@ package com.adner.adnerflix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.adner.adnerflix.databinding.ActivityFormCadastroBinding
 
 class FormCadastro : AppCompatActivity() {
+
+    private lateinit var binding: ActivityFormCadastroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_form_cadastro)
+        binding = ActivityFormCadastroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Esconder a barra de ação padrão
+        supportActionBar!!.hide()
+
+        //Personaliza a toolbar criada com a cor e imagem
+        val toolbar = binding.toolbarCadastro
+        toolbar.setBackgroundColor(getColor(R.color.white))
+        toolbar.setNavigationIcon(getDrawable(R.drawable.ic_logo_svg))
+
     }
 }

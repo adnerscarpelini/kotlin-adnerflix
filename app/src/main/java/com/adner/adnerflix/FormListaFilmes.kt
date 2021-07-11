44a07e2229138f41cb2be4abf0c100d3048159c4
Adner Scarpelini <adner.scarpelini@gmail.com>
2021-07-11 13:23:33 -0300

    Efetuar login no firebase

---
+++ app/src/main/java/com/adner/adnerflix/FormListaFilmes.kt
@@ -0,0 +1,11 @@
+package com.adner.adnerflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormListaFilmes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_lista_filmes)
+    }
+}
--- app/src/main/java/com/adner/adnerflix/FormLogin.kt
+++ app/src/main/java/com/adner/adnerflix/FormLogin.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.Toast
 import com.adner.adnerflix.databinding.ActivityFormLoginBinding
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 
 class FormLogin : AppCompatActivity() {
 
@@ -19,11 +23,63 @@ class FormLogin : AppCompatActivity() {
         //Remover a barra de status
         supportActionBar!!.hide()
 
+        //Se tiver usuario ja logado, ja chama a tela de filmes
+        VerificarUsuarioLogado()
 
-        binding.txtTelaCadastro.setOnClickListener{
-            val intent = Intent(this,FormCadastro::class.java)
+        binding.txtTelaCadastro.setOnClickListener {
+            val intent = Intent(this, FormCadastro::class.java)
             startActivity(intent)
             finish()
         }
+
+        binding.btnEntrar.setOnClickListener {
+            val email = binding.txtEmail.text.toString()
+            val senha = binding.txtSenha.text.toString()
+            val mensagem_erro = binding.txtErro
+
+            if (email.isEmpty() || senha.isEmpty()) {
+                mensagem_erro.setText("Informe o E-mail e Senha")
+            } else {
+                AutenticarUsuario()
+            }
+        }
+    }
+
+    private fun AutenticarUsuario() {
+        val email = binding.txtEmail.text.toString()
+        val senha = binding.txtSenha.text.toString()
+        val mensagem_erro = binding.txtErro
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_SHORT).show()
+                ChamarTelaFilmes()
+            }
+        }.addOnFailureListener {
+
+            var erro = it
+
+            when {
+                erro is FirebaseAuthInvalidCredentialsException -> mensagem_erro.setText("E-mail ou Senha inválidos")
+                erro is FirebaseNetworkException -> mensagem_erro.setText("Sem conexão com a internet")
+                else -> mensagem_erro.setText("Erro ao efetuar LogIn")
+            }
+
+        }
+    }
+
+    private fun VerificarUsuarioLogado(){
+        //Obter o usuário logado no firebase
+        val usuarioLogado = FirebaseAuth.getInstance().currentUser
+
+        if (usuarioLogado != null){
+            ChamarTelaFilmes()
+        }
+    }
+
+    private fun ChamarTelaFilmes() {
+        val intent = Intent(this, FormListaFilmes::class.java)
+        startActivity(intent)
+        finish()
     }
 }
